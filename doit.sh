c7539db332e97653fdbb8623a71598e2674f60e0
Pete Cheslock <petecheslock@gmail.com>
2024-01-19 10:53:44 -0500

    Update notes for running app.

--- doit.sh
+++ doit.sh
@@ -1,15 +1,17 @@
-docker compose up -d postgres
-
 python -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -e .\[test\]
 pip install -r requirements.txt
 
-export DATABASE_PORT=5432
-export DATABASE_HOST=localhost
-export DATABASE_PASSWORD=postgres
-export DATABASE_USER=postgres
-
 # Run Tests
-pytest
+# docker compose up -d postgres
+# export DATABASE_PORT=5432
+# export DATABASE_HOST=localhost
+# export DATABASE_PASSWORD=postgres
+# export DATABASE_USER=postgres
+# pytest 
+
+# Run Sandbox Site
+# make sandbox
+# sandbox/manage.py runserver
